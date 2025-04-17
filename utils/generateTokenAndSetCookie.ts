44967255b57347df00a88e2a5c5b8d59f716c330
Nawarat <110017080+NawratvPatnantaporn@users.noreply.github.com>
2025-04-18 03:19:41 +0700

    Update generateTokenAndSetCookie.ts

--- utils/generateTokenAndSetCookie.ts
+++ utils/generateTokenAndSetCookie.ts
@@ -5,6 +5,11 @@ export const generateTokenAndSetCookie = (
   res: Response,
   userId: string
 ): string => {
+
+  if (!process.env.JWT_SECRET) {
+    throw new Error("JWT_SECRET is not defined in environment variables");
+  }
+  
   const token = jwt.sign({ userId }, process.env.JWT_SECRET as string, {
     expiresIn: "1h",
   });
